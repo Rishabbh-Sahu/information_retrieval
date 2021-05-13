3bdcd4e760a9a2cb85218e71f7ac98d139dadb8a
Rishabbh-sahu <hrishabbh10@gmail.com>
2021-05-14 00:13:24 +0530

    comments clean up

--- text_preprocessing/vectorizer.py
+++ text_preprocessing/vectorizer.py
@@ -10,10 +10,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 class TFIDF:
 
     def __init__(self,max_features=30000):
-        '''
-        model_layer: Model layer to be used to create the tokenizer for
-        max_seq_length: int - maximum number of tokens to keep in a sequence
-        '''
+
         super(TFIDF,self).__init__()
         self.max_features = max_features
         self.tfidf_vectorizor = TfidfVectorizer(decode_error='replace', strip_accents='unicode', analyzer='word',
@@ -21,15 +18,11 @@ class TFIDF:
                                                 norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True)
 
     def create_tfidf_features(self,corpus):
-        '''
+        """
         Creates a tf-idf matrix for the input corpus of data
         param corpus: input text/documents collection
         return: tfidf matrix
-        '''
-        #     tfidf_vectorizor = TfidfVectorizer(decode_error='replace', strip_accents='unicode', analyzer='word',
-        #                                        stop_words='english', ngram_range=(1, 1), max_features=max_features,
-        #                                        norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
-        #                                        max_df=max_df, min_df=min_df)
+        """
         tfidf_mat = self.tfidf_vectorizor.fit_transform(corpus)
         print('tfidf matrix successfully created.')
         return tfidf_mat
@@ -47,7 +40,7 @@ class TFIDF:
         return (most_similar_doc_indices, cosine_similarities)
 
     def show_similar_texts(self,df, cosine_similarities, similar_doc_indices):
-        """ Prints the most similar documents using indices in the `similar_doc_indices` vector."""
+        """ Prints the most similar texts/documents/queries using indices in the `similar_doc_indices` vector."""
         counter = 1
         for index in similar_doc_indices:
             print('Top-{}, Similarity = {}'.format(counter, cosine_similarities[index]))
