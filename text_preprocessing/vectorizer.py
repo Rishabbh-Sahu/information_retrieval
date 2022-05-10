f4b4dc49e0498cbe9eadafc6489a4a52337ad6d5
Rishabbh <69572197+Rishabbh-Sahu@users.noreply.github.com>
2022-05-10 22:12:13 +0100

    docstring updated

--- text_preprocessing/vectorizer.py
+++ text_preprocessing/vectorizer.py
@@ -10,7 +10,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 class TFIDF:
     """
     This class helps in representing text to their numeric equivalent by spliting stream of text into tokens (words) and 
-    using their normalize values (L2 in this case) to create the numberic matrics. 
+    using their normalize values (L2 in this case) to create the numberic matrics
     """
     def __init__(self,max_features=50000):
 
@@ -31,9 +31,10 @@ class TFIDF:
         return tfidf_mat
 
     def calculate_similarity(self, tfidf_mat, text_str, top_k=10):
-        """ Vectorizes the text/document via `vectorizor` and calculates the cosine similarity of
-        the text/document and tfidf_mat (all the documents) and returns the `top_k` similar documents."""
-
+        """ 
+        Vectorizes the text/document via `vectorizor` and calculates the cosine similarity of
+        the text/document and tfidf_mat (all the documents) and returns the `top_k` similar documents
+        """
         # Vectorize the query to the same length as documents
         text_vec = self.tfidf_vectorizor.transform(text_str)
         # Compute the cosine similarity between text_vec and all the texts/documents
@@ -43,7 +44,7 @@ class TFIDF:
         return (most_similar_doc_indices, cosine_similarities)
 
     def show_similar_texts(self,df, cosine_similarities, similar_doc_indices):
-        """ Prints the most similar texts/documents/queries using indices in the `similar_doc_indices` vector."""
+        """ Prints the most similar texts/documents/queries using indices in the `similar_doc_indices` vector"""
         counter = 1
         for index in similar_doc_indices:
             print('Top-{}, Similarity = {}'.format(counter, cosine_similarities[index]))
